3793b04075f4169505ead0ca6a102a69c2d4da04
Elijah Zarezky <Elijah Zarezky@9a190745-9f41-0410-876d-23307a9d09e3>
2005-07-16 18:23:32 +0000

    Slightly reformatted source code.

--- SubstSvc/Source/stdafx.h
+++ SubstSvc/Source/stdafx.h
@@ -45,8 +45,8 @@ void WINAPI ServiceHandler(DWORD fdwControl);
 void WINAPI ServiceMain(DWORD dwArgc, LPTSTR apszArgv[]);
 
 // force ISO/IEC 14882 conformance in for loop scope
-#if _MSC_VER < 1300
-#define for if(false); else for
+#if (_MSC_VER < 1300)
+#define for if (false); else for
 #else
 #pragma conform(forScope, on)
 #endif	// _MSC_VER
--- WinSubst/Source/stdafx.h
+++ WinSubst/Source/stdafx.h
@@ -39,8 +39,8 @@
 #include "../../../Repository/CodeProject/Source/FolderDlg.h"
 
 // force ISO/IEC 14882 conformance in for loop scope
-#if _MSC_VER < 1300
-#define for if(false); else for
+#if (_MSC_VER < 1300)
+#define for if (false); else for
 #else
 #pragma conform(forScope, on)
 #endif	// _MSC_VER
