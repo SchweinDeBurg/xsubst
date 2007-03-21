5448e4ba120958c49a3f3e509e1854516bf3befa
elijah <elijah@9a190745-9f41-0410-876d-23307a9d09e3>
2007-03-21 16:32:55 +0000

    Tagged release 1.0.2800.

--- WinSubst/Source/stdafx.h
+++ WinSubst/Source/stdafx.h
@@ -38,13 +38,6 @@
 #include "../../../Repository/AfxGadgets/Source/Win32Error.h"
 #include "../../../Repository/CodeProject/Source/FolderDlg.h"
 
-// force ISO/IEC 14882 conformance in for loop scope
-#if (_MSC_VER < 1300)
-#define for if (false); else for
-#else
-#pragma conform(forScope, on)
-#endif	// _MSC_VER
-
 #endif	// __stdafx_h
 
 // end of file
