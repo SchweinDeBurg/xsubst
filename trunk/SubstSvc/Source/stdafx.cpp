24c92e1dae57504dfb15d24295e26b1726143be7
Elijah Zarezky <Elijah Zarezky@9a190745-9f41-0410-876d-23307a9d09e3>
2010-07-16 11:10:30 +0000

    Slightly reformatted source code.

--- trunk/SubstSvc/Source/stdafx.cpp
+++ trunk/SubstSvc/Source/stdafx.cpp
@@ -69,6 +69,13 @@ DWORD g_dwServiceState = SERVICE_STOPPED;
 //////////////////////////////////////////////////////////////////////////////////////////////
 // manifest dependencies
 
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+// VC-2008 RTM                       9.0.21022.8
+// VC-2008 Feature Pack              9.0.30411.0
+// VC-2008 SP1                       9.0.30729.1
+// VC-2008 SP1 ATL Security Update   9.0.30729.4148
+
 #if defined(_DEBUG)
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugCRT' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugMFC' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
@@ -76,6 +83,5 @@ DWORD g_dwServiceState = SERVICE_STOPPED;
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.PrivateCRT' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.PrivateMFC' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #endif   // _DEBUG
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 // end of file
--- trunk/WinSubst/Source/stdafx.cpp
+++ trunk/WinSubst/Source/stdafx.cpp
@@ -66,6 +66,13 @@ static char THIS_FILE[] = __FILE__;
 //////////////////////////////////////////////////////////////////////////////////////////////
 // manifest dependencies
 
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+// VC-2008 RTM                       9.0.21022.8
+// VC-2008 Feature Pack              9.0.30411.0
+// VC-2008 SP1                       9.0.30729.1
+// VC-2008 SP1 ATL Security Update   9.0.30729.4148
+
 #if defined(_DEBUG)
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugCRT' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugMFC' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
@@ -73,6 +80,5 @@ static char THIS_FILE[] = __FILE__;
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.PrivateCRT' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.PrivateMFC' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #endif   // _DEBUG
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 // end of file
